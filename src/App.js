e8afa40e5e442db5cd893c05f4e83f73e1316102
Mayuri Sune <sunemayuri98@gmail.com>
2024-12-25 21:49:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import InfiniteScroll from './Components/InfiniteScroll';
+
+function App() {
+  return (
+    <div>
+     <InfiniteScroll/>
+    </div>
+  );
+}
+
+export default App;
